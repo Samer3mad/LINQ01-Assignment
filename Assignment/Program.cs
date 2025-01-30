0d4efe03bd0be57fd816599d3e43db77bac225a4
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2025-01-30 17:44:02 +0200

    LINQ - Restriction Operators [Question 01]

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Assignment
+﻿using static Assignment.ListGenerator;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            #region LINQ - Restriction Operators
+            #region Question 01
+            var res = ProductList.Where(P => P.UnitsInStock == 0);
+
+            var Res = from p in ProductList
+                  where p.UnitsInStock == 0
+                  select p;
+            foreach (var item in Res)
+            {
+                Console.WriteLine(item);
+            }
+                #endregion
+                #endregion
         }
     }
 }
